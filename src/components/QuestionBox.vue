45864a6a829f7124f4407085653a23a73fee29ed
Rob <gio@llnnll.com>
2019-10-24 13:33:47 -0400

    generated API link for sample data

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="question-box">
+    <b-jumbotron header="Bootstrap Vue" lead="Bootstrap 4 Components for VueJS 2">
+      <template slot="lead">
+        Some Question
+      </template>
+
+      <hr class="my-4" />
+
+      <p>
+        List of answers
+      </p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
